ec1312461c5fa88b3a00edf17457ea135319a4f6
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-11-14 18:24:34 -0500

    Add shader paths & Rubik movements definitions

--- include/utils.h
+++ include/utils.h
@@ -1,19 +1,38 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#define SCR_TITLE						"Rayver - OpenGL Project"
-#define SCR_WIDTH						800
-#define SCR_HEIGHT						600
+#define SCR_TITLE							"Rayver - OpenGL Project"
+#define SCR_WIDTH							800
+#define SCR_HEIGHT							600
 
-#define X_AXIS							0
-#define Y_AXIS							1
-#define Z_AXIS							2
+#define X_AXIS								0
+#define Y_AXIS								1
+#define Z_AXIS								2
 
-#define PI								3.14159265359
+#define PI									3.14159265359
 #define to_radians(x)						(x * (float) PI ) / 180.0f
 
-#define FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\fragment_shader.fs"
-#define VERTEX_SHADER_PATH				"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\vertex_shader.vs"
+#define GREEN_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\green.fs"
+#define WHITE_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\white.fs"
+#define BLACK_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\black.fs"
+#define BLUE_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\blue.fs"
+#define YELLOW_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\yellow.fs"
+#define RED_FRAGMENT_SHADER_PATH			"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\red.fs"
+#define FRAGMENT_SHADER_PATH				"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\fragment_shader.fs"
+#define VERTEX_SHADER_PATH					"..\\..\\..\\..\\..\\src\\Tutorial_Rubik\\shaders\\vertex_shader.vs"
+
+
+#define F_MOVEMENT							'F'
+#define D_MOVEMENT							'D'
+#define U_MOVEMENT							'U'
+#define B_MOVEMENT							'B'
+#define R_MOVEMENT							'R'
+#define L_MOVEMENT							'L'
+#define M_MOVEMENT							'M'
+#define E_MOVEMENT							'E'
+#define S_MOVEMENT							'S'
+
+
 
 
 #endif // __UTILS_H__
