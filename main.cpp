ed9a7c9a0216aaa68a00acffe088c17fb672cae8
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-11-21 10:26:28 -0500

    Change include directories & add external movements definition to access to M(), E(), S() Rubik struct methods

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "./include/open_gl_loader.h"
 #include "./include/rubik.h"
-#include "./solver/solver.h"
+#include "./include/solver/solve.h"
+#include "./include/solver/random.h"
+#include "./include/menu.h"
+
+//#define __EXTERNAL_MOVEMENTS__
 
 using namespace std;
 
@@ -12,11 +16,11 @@ Point center(0.2f, 0.2f, 0.0f);
 Rubik *myRubik;
 
 void processInput(GLFWwindow*);
-std::string get_solution(std::string);
+std::string get_reversed(std::string);
 
 OpenGlLoader OpenGL(SCR_WIDTH, SCR_HEIGHT);
 std::string history;
-std::string solution, reverse_solution;
+std::string moves, reverse_solution;
 
 int prime = 0;
 int write = 0;
@@ -25,8 +29,10 @@ int main()
 {
 
 	myRubik = new Rubik(center, separation);
-	solution = "BDUDLUF";
-	std::cout << solver::format_movements(solution) << std::endl;
+	moves = "LU";
+	std::vector<std::string> a = get_solution(to_cube_not(string_to_vector(moves)));
+	std::string b = format_solution(a);
+	menu();
 
 	while (OpenGL.isOpen())
 	{
@@ -58,121 +64,134 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 	if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
 	{
-		std::cout << solution << std::endl;
-		reverse_solution.append(solution);
-		myRubik->read_solution(solution);
+		std::cout << moves << std::endl;
+		reverse_solution.append(moves);
+		myRubik->read_solution(moves);
 		myRubik->set_speed(1.5f);
 		myRubik->enable();
-		history.append(solution);
-		solution.clear();
+		history.append(moves);
+		moves.clear();
 	}
 	
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
 	{
-		std::string perfect_solution = solver::solve(solver::format_movements(history));
-		std::cout << "perfect solution: " << perfect_solution << std::endl;
-		std::cout << "format solution: " << solver::format_solution(perfect_solution) << std::endl;
-		//solution = get_solution(reverse_solution);
-		myRubik->read_solution(solver::format_solution(perfect_solution));
-		reverse_solution.clear();
-		history.clear();
+		if (!history.empty())
+		{
+			std::vector<std::string> a = get_solution(to_cube_not(string_to_vector(history)));
+			std::string b = format_solution(a);
+			for (auto e : a)
+			{
+				std::cout << e << std::endl;
+			}
+			std::cout << b << std::endl;
+
+			myRubik->read_solution(b);
+			reverse_solution.clear();
+			history.clear();
+		}
 	}
 
 	if (prime)
 	{
 		if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
 		{
-			solution.push_back(F_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(F_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
 		{
-			solution.push_back(B_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(B_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
 		{
-			solution.push_back(U_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(U_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		{
-			solution.push_back(D_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(D_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
 		{
-			solution.push_back(R_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(R_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
 		{
-			solution.push_back(L_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(L_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
+#ifdef __EXTERNAL_MOVEMENTS__
 		if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
 		{
-			solution.push_back(M_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(M_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
 		{
-			solution.push_back(E_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(E_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
 		{
-			solution.push_back(S_PRIME_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(S_PRIME_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
+#endif // __EXTERNAL_MOVEMENTS__
+	
 	}
 	else
 	{
 		if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
 		{
-			solution.push_back(F_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(F_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
 		{
-			solution.push_back(B_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(B_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
 		{
-			solution.push_back(U_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(U_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		{
-			solution.push_back(D_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(D_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
 		{
-			solution.push_back(R_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(R_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
 		{
-			solution.push_back(L_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(L_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
+#ifdef __EXTERNAL_MOVEMENTS__
+
 		if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
 		{
-			solution.push_back(M_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(M_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
 		{
-			solution.push_back(E_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(E_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
 		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
 		{
-			solution.push_back(S_MOVEMENT);
-			std::cout << solution << std::endl;
+			moves.push_back(S_MOVEMENT);
+			std::cout << moves << std::endl;
 		}
+#endif // __EXTERNAL_MOVEMENTS__
 	}
 }
 
@@ -219,10 +238,10 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 	OpenGL.camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
-std::string get_solution(std::string all_moves)
+std::string get_reversed(std::string all_moves)
 {
 	std::string ans;
-	for (int i = all_moves.size() - 1; ~i; i--)
+	for (size_t i = all_moves.size() - 1; ~i; i--)
 	{
 		switch (all_moves[i])
 		{
