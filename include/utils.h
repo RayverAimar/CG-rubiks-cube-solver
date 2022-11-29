ba90ca922fcc78622989712cc627ffe60eb7f79d
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-11-29 14:53:47 -0500

    Delete M, E, S movements and idx litters

--- include/utils.h
+++ include/utils.h
@@ -28,9 +28,6 @@
 #define B_MOVEMENT							'B'
 #define R_MOVEMENT							'R'
 #define L_MOVEMENT							'L'
-#define M_MOVEMENT							'M'
-#define E_MOVEMENT							'E'
-#define S_MOVEMENT							'S'
 
 #define F_PRIME_MOVEMENT					'f'
 #define D_PRIME_MOVEMENT					'd'
@@ -38,10 +35,6 @@
 #define B_PRIME_MOVEMENT					'b'
 #define R_PRIME_MOVEMENT					'r'
 #define L_PRIME_MOVEMENT					'l'
-#define M_PRIME_MOVEMENT					'm'
-#define E_PRIME_MOVEMENT					'e'
-#define S_PRIME_MOVEMENT					's'
-
 
 const char valid_moves[] = { F_MOVEMENT, D_MOVEMENT, U_MOVEMENT, B_MOVEMENT, R_MOVEMENT, L_MOVEMENT,
 F_PRIME_MOVEMENT, D_PRIME_MOVEMENT, U_PRIME_MOVEMENT, B_PRIME_MOVEMENT, R_PRIME_MOVEMENT, L_PRIME_MOVEMENT };
@@ -54,9 +47,5 @@ const unsigned int up_litter_idx[]		= { 6, 7, 8, 15, 16, 17, 24, 25, 26 };
 const unsigned int down_litter_idx[]	= { 18, 19, 20, 9, 10, 11, 0, 1, 2 };
 const unsigned int left_litter_idx[]	= { 2, 11, 20, 5, 14, 23, 8, 17, 26 };
 const unsigned int right_litter_idx[]	= { 0, 3, 6, 9, 12, 15, 18, 21, 24 };
-const unsigned int middle_litter_idx[]	= { 1, 4, 7, 10, 13, 16, 19, 22, 25 };
-const unsigned int e_litter_idx[]		= { 3, 4, 5, 12, 13, 14, 21, 22, 23 };
-const unsigned int s_litter_idx[]		= { 9, 10, 11, 12, 13, 14, 15, 16, 17 };
-
 
 #endif // __UTILS_H__
